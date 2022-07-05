f45c289ee4f2883e7caa8537bcb261667a133896
Evaldo <evaldocop@yahoo.com.br>
2022-07-05 14:35:42 -0300

    configuração criptografia

--- demo-security/demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ demo-security/demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.mballem.curso.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.mballem.curso.security.service.UsuarioService;
 
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Autowired
+	private UsuarioService usuarioService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -27,6 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	
+
+	
+			
+			
 	
 	
 
